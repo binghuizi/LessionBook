a7d8cd167f12e3ff738d34be6e5a63b372476d52
赵佳慧--芒果iOS <1146623752@qq.com>
2016-03-16 18:47:27 +0800

    316下午赵2

---
+++ LessionBook/Class/Discover/Models/TypeModel.h
@@ -0,0 +1,17 @@
+//
+//  TypeModel.h
+//  LessionBook
+//
+//  Created by scjy on 16/3/16.
+//  Copyright © 2016年 迪士尼工作室. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TypeModel : NSObject
+@property(nonatomic,strong) NSString *listorder;
+@property(nonatomic,strong) NSString *name;
+@property(nonatomic,strong) NSString *parentid;
+@property(nonatomic,strong) NSString *Category;
+@property(nonatomic,strong) NSString *thumb;
+@end
