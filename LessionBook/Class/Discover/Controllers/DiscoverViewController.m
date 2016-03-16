2f09fd0042cc68b867fb19d1a7b0ac64a0d9ce1d
赵佳慧--芒果iOS <1146623752@qq.com>
2016-03-16 11:32:15 +0800

    2016年3.16赵

--- LessionBook/Class/Discover/Controllers/DiscoverViewController.m
+++ LessionBook/Class/Discover/Controllers/DiscoverViewController.m
@@ -7,9 +7,9 @@
 //
 
 #import "DiscoverViewController.h"
-
+#import "VOSegmentedControl.h"
 @interface DiscoverViewController ()
-
+@property(nonatomic,retain) VOSegmentedControl *segment1;
 @end
 
 @implementation DiscoverViewController
@@ -17,21 +17,48 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self.view addSubview:self.segment1];
 }
+//懒加载
+-(VOSegmentedControl *)segment1{
+    if (_segment1 == nil) {
+        
+                self.segment1 = [[VOSegmentedControl alloc]initWithSegments:@[@{VOSegmentText:@"分类"},
+                                                                      @{VOSegmentText:@"精选"}]];
+        self.segment1.textColor = [UIColor blackColor];
+        self.segment1.selectedTextColor = [UIColor orangeColor];
+        self.segment1.contentStyle = VOContentStyleTextAlone;
+        self.segment1.indicatorStyle = VOSegCtrlIndicatorStyleTopLine;
+        
+        self.segment1.allowNoSelection = NO;
+        self.segment1.frame = CGRectMake(0,60,kWideth, 44);
+
+        
+    }
+    return _segment1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
