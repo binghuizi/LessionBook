9275df12988961410e77711e79c250b818920efd
赵佳慧--芒果iOS <1146623752@qq.com>
2016-03-16 22:04:51 +0800

    精选316晚上

---
+++ LessionBook/Class/Discover/Models/ChoseModel.h
@@ -0,0 +1,15 @@
+//
+//  ChoseModel.h
+//  LessionBook
+//
+//  Created by scjy on 16/3/16.
+//  Copyright © 2016年 迪士尼工作室. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ChoseModel : NSObject
+@property(nonatomic,strong) NSString *catName;//分类标题
+@property(nonatomic,strong) NSString *bigthumb;//图片
+@property(nonatomic,strong) NSString *name;    //书名
+@end
---
+++ LessionBook/Class/Discover/Views/ChoseCollectionViewCell.m
@@ -0,0 +1,17 @@
+//
+//  ChoseCollectionViewCell.m
+//  LessionBook
+//
+//  Created by scjy on 16/3/16.
+//  Copyright © 2016年 迪士尼工作室. All rights reserved.
+//
+
+#import "ChoseCollectionViewCell.h"
+
+@implementation ChoseCollectionViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end
