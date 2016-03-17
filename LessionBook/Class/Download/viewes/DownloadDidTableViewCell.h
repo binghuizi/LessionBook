83e8d4dee58ed5659ebab12e0e10b19869bd806f
WangXiaoQi <WangXiaoQi@gmail.com>
2016-03-17 10:10:32 +0800

    UIVIEW类目

---
+++ LessionBook/Class/Download/viewes/DownloadDidTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  DownloadDidTableViewCell.h
+//  LessionBook
+//
+//  Created by scjy on 16/3/17.
+//  Copyright © 2016年 迪士尼工作室. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DownloadDidTableViewCell : UITableViewCell
+
+@end
